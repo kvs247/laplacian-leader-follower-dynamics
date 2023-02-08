79b2fa9d10c29f487a2861de9f6a3059979afb13
Kyle Schneider <kyle.v.schneider.251@gmail.com>
2023-02-08 14:47:04 -0700

    cleaned up

--- plot.py
+++ plot.py
@@ -28,26 +28,17 @@ for index, (key, value) in enumerate(data.items()):
     yCUDEGEN[index] = value['CU-degen'] / numCUMatrices
     yCUNONDEGEN[index] = value['CU-nondegen'] / numCUMatrices
 
-# yCU = [sum(x) for x in zip(yCUDIS, yCUDEGEN, yCUNONDEGEN)]
-
-print(yEC)
-print(yCC)
-print(yCU)
-print('===')
-print(yCUDIS)
-print(yCUDEGEN)
-print(yCUNONDEGEN)
-
-plt.plot(x, yEC, label='ec')
-plt.plot(x, yCC, label='cc')
-plt.plot(x, yCU, label='cu')
+plt.plot(x, yEC, label='ec', color='green')
+plt.plot(x, yCC, label='cc', color='blue')
+plt.plot(x, yCU, label='cu', color='purple')
 plt.legend()
 plt.savefig('fig.png')
+plt.show()
 
 plt.close()
 
-plt.plot(x, yCUDIS, label='cu-dis')
-plt.plot(x, yCUDEGEN, label='cu-degen')
-plt.plot(x, yCUNONDEGEN, label='cu-nondegen')
+plt.plot(x, yCUDIS, label='cu-dis', color='blue')
+plt.plot(x, yCUDEGEN, label='cu-degen', color='purple')
+plt.plot(x, yCUNONDEGEN, label='cu-nondegen', color='red')
 plt.legend()
 plt.savefig('fig2.png')
